3d6508418ec18a48b940d4f430dfe59975a99bd7
Alejandro Lopez <lopalejandro@gmail.com>
2023-04-21 15:05:46 -0300

    cambio intermedio

--- js/mijs.js
+++ js/mijs.js
@@ -1,7 +1,42 @@
 function validateForm() {
-  const form = document.querySelector("#cto-frm");
-  const submitBtn = document.querySelector("#btn-submit");
 
+  const btnEnviar = document.getElementById('btn-submit');
+
+  const validación = (e) => {
+    e.preventDefault();
+    
+    const nombreDeUsuario = document.getElementById('nombre');
+    const direcciónEmail = document.getElementById('email');
+    if (nombre.value === "") {
+      alert("Por favor, escribe tu nombre de usuario.");
+      nombre.focus();
+      return false;
+    }
+      
+    if (email.value === "") {
+      alert("Por favor, escribe tu correo electrónico");
+      email.focus();
+      return false;
+    }
+  
+    if (!emailVálido(email.value)) {
+      alert("Por favor, escribe un correo electrónico válido");
+      email.focus();
+      return false;
+    }
+    
+    return true; //Se pueden enviar los datos del formulario al servidor
+  }
+  
+  const emailVálido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+  
+  submitBtn.addEventListener('click', validate);
+
+  
+  /* const form = document.querySelector("#cto-frm");
+  
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -19,8 +54,8 @@ function validateForm() {
       return;
     }
 
-    form.submit();
-  });
+    /*form.submit();
+  }); */
 }
 
 function isValidEmail(email) {
