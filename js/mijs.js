9812d38314462137d34fccb14f757c6ed43a745e
Alejandro Lopez <lopalejandro@gmail.com>
2023-04-24 14:06:24 -0300

    Casi terminada la logica del formulario. Se arreglaron detalles en el css

--- js/mijs.js
+++ js/mijs.js
@@ -1,65 +1,92 @@
-function validateForm() {
+document.addEventListener("DOMContentLoaded", function () {
+  var formulario = document.getElementById("cto_frm");
 
-  const btnEnviar = document.getElementById('btn-submit');
-
-  const validación = (e) => {
-    e.preventDefault();
-    
-    const nombreDeUsuario = document.getElementById('nombre');
-    const direcciónEmail = document.getElementById('email');
-    if (nombre.value === "") {
-      alert("Por favor, escribe tu nombre de usuario.");
-      nombre.focus();
+  var validarNombre = function (e) {
+    if (formulario.nombre.value.length < 10) {
+      alert("Por favor, escribe tu nombre y apellido.");
+      document.getElementById("nombre").focus();
       return false;
     }
-      
-    if (email.value === "") {
-      alert("Por favor, escribe tu correo electrónico");
-      email.focus();
-      return false;
-    }
-  
-    if (!emailVálido(email.value)) {
-      alert("Por favor, escribe un correo electrónico válido");
-      email.focus();
-      return false;
-    }
-    
-    return true; //Se pueden enviar los datos del formulario al servidor
-  }
-  
-  const emailVálido = email => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
-  
-  submitBtn.addEventListener('click', validate);
+    return true;
+  };
 
-  
-  /* const form = document.querySelector("#cto-frm");
-  
-  form.addEventListener("submit", function (event) {
+  var validarDireccion = function (e) {
+    if (formulario.direccion.value.length < 10) {
+      alert("Por favor, escribe tu dirección.");
+      document.getElementById("direccion").focus();
+      return false;
+    }
+    return true;
+  };
+
+  var validarCiudad = function (e) {
+    if (formulario.ciudad.value.length < 10) {
+      alert("Por favor, escribe el nombre de tu ciudad.");
+      document.getElementById("ciudad").focus();
+      return false;
+    }
+    return true;
+  };
+
+  var validarCodigo = function (e) {
+    var cp = document.getElementById("codigo").value;
+    if (isNaN(cp) || cp < 1000 || cp > 9999) {
+      alert("Por favor, escribe tu código postal correctamente.");
+      document.getElementById("codigo").focus();
+      return false;
+    }
+    return true;
+  };
+
+  var validarCorreo = function (e) {
+    var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+    var email = document.getElementById("email").value;
+    if (!expresion.test(email)) {
+      alert("Por favor, escribe un email válido.");
+      document.getElementById("email").focus();
+      return false;
+    }
+    return true;
+  };
+
+  var validarMensaje = function () {
+    if (formulario.mensaje.value.length == 0) {
+      alert("Por favor, escribe algun mensaje válido.");
+      document.getElementById("mensaje").focus();
+      return false;
+    }
+    return true;
+  };
+
+  var validar = function () {
+    return (
+      validarNombre() &&
+      validarDireccion()&&
+      validarCiudad()&&
+      validarCodigo()&&
+      validarCorreo() &&
+      validarMensaje()
+    );
+  };
+
+  formulario.addEventListener("submit", handleSubmit);
+  async function handleSubmit(event) {
     event.preventDefault();
-
-    const nombre = document.querySelector("#nombre").value;
-    const email = document.querySelector("#email").value;
-    const direccion = document.querySelector("#direccion").value;
-
-    if (!nombre || !email || !direccion) {
-      alert("Por favor rellene todos los campos.");
-      return;
+    if (validar()) {
+      const form = new FormData(this);
+      const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      if (response.ok) {
+        this.reset();
+        alert(
+          "Gracias por contactarte con nosotros. En breve nos estaremos comunicando."
+        );
+      }
     }
-
-    if (!isValidEmail(email)) {
-      alert("Por favor ingrese un email válido.");
-      return;
-    }
-
-    /*form.submit();
-  }); */
-}
-
-function isValidEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  return emailRegex.test(email);
-}
+  }
+});
